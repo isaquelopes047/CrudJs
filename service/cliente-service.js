d9fe6120946db93f72868a0f7b8cbbe1b5d8a2ac
Isaquelopes91 <isaque.lopes047@gmail.com>
2022-01-06 10:38:13 -0300

    add

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,47 +1,12 @@
 
-const criarNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr');
-
-    // Colocamos o conteudo a ser criado dentro de uma constante
-    const conteudo = 
-    `
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td>   
-    `
-    // Pegamos a const criada acima e passamos a ela a const conteudo e exibimos com o innerHTML
-    linhaNovoCliente.innerHTML = conteudo;
-    return linhaNovoCliente
-}
-const tabela = document.querySelector('[data-tabela]')
-
-
 //Estamos criando uma instancia para consumir dados para nossa aplicação
 const listaDeCliente = () => {
-    const promise = new Promise((resolve, reject) => {
-
-        const http = new XMLHttpRequest()
-        const url = "http://localhost:3000/profile"
-
-        http.open('GET', url); 
-        http.send();
-
-        http.onload = () => {
-            const data = JSON.parse(http.response) 
-            data.forEach(elemento => {
-                tabela.appendChild(criarNovaLinha(elemento.nome, elemento.email))
-            });
-        }
+    return fetch(`http://localhost:3000/profile`)
+    .then(resposta => {
+        return resposta.json();
     })
+}
 
-// Vai apresentar as informaçoes na aplicação fronthttp.onload = () => {
-    const data = JSON.parse(http.response)
-    data.forEach(elemento => {
-        tabela.appendChild(criarNovaLinha(elemento.nome, elemento.email))
-    });
+export const clienteService = {
+    listaDeCliente
 }
