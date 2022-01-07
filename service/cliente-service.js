4c0d8f04f9e9fb32450829449b98aa8b5305475a
Yoona Blaska <isaque.lopes047@gmail.com>
2022-01-07 20:25:46 -0300

    add

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -28,9 +28,34 @@ const removeCliente = (id) => {
     })
 }
 
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then(resposta => {
+        return resposta.json()
+    })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+        .then( resposta => {
+            return resposta.json()
+        })
+    })
+}
+
 export const clienteService = {
     listaDeCliente,
     criaCliente,
-    removeCliente
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 }
 
